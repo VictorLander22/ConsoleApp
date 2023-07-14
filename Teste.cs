0c20ef6e1be75c7945f7b6d47a72c33df9c9f659
Victor Daniel Lander <victordlander@hotmail.com>
2023-07-14 09:15:18 -0300

    a

---
+++ Teste.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+
+namespace TesteNamespace
+{
+
+    namespace x
+    {
+        class Teste
+        {
+            string nome = "";
+            public Teste(string nomeConstrutor)
+            {
+                nome = nomeConstrutor;
+            }
+
+            public void Metodo()
+            {
+                Console.WriteLine($"Ola de outra classe {nome}");
+            }
+
+        }
+    }
+}
