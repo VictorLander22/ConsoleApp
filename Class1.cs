2649f881eaebd6e5fda585636912a53595ec720e
Victor Daniel Lander <victordlander@hotmail.com>
2023-07-18 08:31:49 -0300

    s

---
+++ Class1.cs
@@ -0,0 +1,47 @@
+﻿using PdfSharpCore.Drawing;
+using PdfSharpCore.Pdf;
+
+namespace ConsoleApp
+{
+    public abstract class Pagamento
+    {
+        float valor = 0;
+        bool parcelado = false;
+        short numeroParcela = 0;
+
+        public virtual void Pagar()
+        {
+
+        }
+    }
+
+    class PagamentoBoleto : Pagamento
+    {
+        public static void GerarBoletoPDF(string nomeArquivo, string Sacado)
+        {
+            // Criar o documento PDF
+            PdfDocument document = new();
+            document.Info.Title = "Boleto";
+
+            // Adicionar uma página ao documento
+            PdfPage page = document.AddPage();
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+
+            // Definir fonte e tamanho
+            XFont fonte = new("Arial", 12);
+
+            // Escrever o conteúdo do boleto no PDF
+            gfx.DrawString($"Ola {Sacado} aqui esta seu boleto", fonte, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.TopLeft);
+
+            // Salvar o PDF em um arquivo
+            document.Save(nomeArquivo);
+        }
+
+        public override void Pagar()
+        {
+            string s = "";
+            s += Console.ReadLine();
+            GerarBoletoPDF("boleto.pdf", s);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,19 +5,21 @@ namespace ConsoleApp
 {
     class Program
     {
+        static PagamentoBoleto pBoleto1 = new();
         static void Main(string[] args)
         {
-            Console.Clear();
+            // Console.Clear();
 
             AppPresetation();
             UserInfo();
-            NumberGuesser();
+
+            pBoleto1.Pagar();
         }
 
 
         static void AppPresetation()
         {
-            string appName = "Number Guesser";
+            string appName = "Pagamento console";
             string appVersion = " 1.0.0";
             string appAuthor = "Victor Daniel Lander";
 
@@ -28,56 +30,12 @@ namespace ConsoleApp
         {
             Console.WriteLine("Digite seu nome");
 
-            string userName = Console.ReadLine();
+            string userName = "";
+            userName += Console.ReadLine();
 
-            if (!string.IsNullOrEmpty(userName))
-                Console.WriteLine("Seu nome e {0}", userName);
+            Console.WriteLine("Seu nome é {0}", userName ?? "Desconhecido");
         }
-        static void NumberGuesser()
-        {
-            while (true)
-            {
-                // se for reutilizado mais vezes
-                //Random random = new Random();
-                //   int correctNumber = random.Next(1, 10);
 
-                int correctNumber = new Random().Next(1, 10);
-                int guessNumber = 0;
-
-                Console.WriteLine("Tente adivinhar o numero");
-
-                while (correctNumber != guessNumber)
-                {
-                    //guessNumber = Convert.ToInt32(Console.ReadLine());
-                    // guessNumber = Int32.TryParse(Console.ReadLine());
-                    bool isValid = int.TryParse(Console.ReadLine(), out guessNumber);
-                    if (!isValid)
-                    {
-                        ConsoleCollorPrinter("Por favor digite apenas numeros", ConsoleColor.Red);
-                        continue;// continue dentro do while volta para o inicio do mesmo
-                    }
-
-
-                    if (correctNumber != guessNumber)
-                    {
-                        ConsoleCollorPrinter("Numero errado,tente novamente", ConsoleColor.Red);
-                    }
-                }
-                ConsoleCollorPrinter("Parabens voce acertou o numero", ConsoleColor.Green);
-
-                Console.WriteLine("Quer jogar de novo ? , [Y ou N]");
-
-                string resposta = Console.ReadLine().ToUpper();
-
-                if (resposta == "Y")
-                    continue;
-                else if (resposta == "N")
-                    return;
-                else
-                    return;
-
-            }
-        }
         static void ConsoleCollorPrinter(string text, ConsoleColor c)
         {
             Console.ForegroundColor = c;
@@ -86,5 +44,7 @@ namespace ConsoleApp
 
         }
 
+
     }
+
 }
